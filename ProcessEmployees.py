8d56b93b8e0d1f4cbc5f71bd47f500050877117f
Dianne Reyes <dianne_reyes@baylor.edu>
2023-02-14 13:12:17 -0600

    updated salary

--- ProcessEmployees.py
+++ ProcessEmployees.py
@@ -14,18 +14,35 @@ import csv
 
 #open the file
 
+cust_repr = open("employee_data.csv","r")
 
+repr_infile = csv.reader(cust_repr, delimiter=",")
+
+header = next(repr_infile)
 
 
 #create an empty dictionary
 
+repr_sal_up = {}
 
 #use a loop to iterate through the csv file
 
-
+tot_cur = 0.0
+tot_new = 0.0
+for r in repr_infile:
     #check if the employee fits the search criteria
 
+    if r[9] == 'TS' and r[3] == 'Marketing':
+        
+        emp_name = r[1]+' '+r[2]
+        cur_sal = int(r[5])
+        tot_cur += cur_sal
+        print(f'Employee Name: {emp_name} Current Salary: ${cur_sal:.2f}')
 
+        up_sal = cur_sal + (cur_sal*.10)
+
+        repr_sal_up[emp_name] = up_sal
+        tot_new += up_sal
     
 
 print()
@@ -34,7 +51,8 @@ print()
 
 #iternate through the dictionary and print out the key and value as per image
 
-
+for rep in repr_sal_up:
+    print(f'Employee Name: {rep} New Salary: ${repr_sal_up[rep]:.2f}')
 
 
           
@@ -44,6 +62,7 @@ print('=========================================')
 print()
 
 #print out the total difference between the old salary and the new salary as per image.
+tot_inc = tot_new - tot_cur
 
-        
-    
+print(f'Total Increase in Salary:${tot_inc:.2f}')
+
